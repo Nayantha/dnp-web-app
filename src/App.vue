3965bc8e6f3782b18909ec1ebd4317b93ba558fc
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-04-13 19:00:21 +0530

    add the main component to the app.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <script lang="ts" setup>
 import Header from './components/Header.vue'
 import Footer from "./components/Footer.vue";
+import Main from "./components/Main.vue";
 </script>
 
 <template>
-    <div class="w-11/12 mx-auto min-h-screen flex flex-col dark:bg-black bg-white">
+    <div class="md:w-11/12 mx-auto min-h-screen flex flex-col dark:bg-black bg-white">
         <Header/>
-        <main class="flex-grow">
-            <slot />
-        </main>
+        <Main/>
         <Footer/>
     </div>
 </template>
