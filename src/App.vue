1e95855a4af018cee99aee588a50559b247d3939
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-04-13 13:55:50 +0530

    add the footer ad main slot to the app.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <script lang="ts" setup>
 import Header from './components/Header.vue'
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
-    <div class="w-11/12 mx-auto dark:bg-black bg-white">
+    <div class="w-11/12 mx-auto min-h-screen flex flex-col dark:bg-black bg-white">
         <Header/>
+        <main class="flex-grow">
+            <slot />
+        </main>
+        <Footer/>
     </div>
 </template>
 
